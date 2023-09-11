9268300557e1944d281fad522e316d7cdaf3d7ef
Fawaznasrudeen <nasrudeenfawaz@gmail.com>
2023-09-11 20:50:13 +0100

    new file

--- diceGame.js
+++ diceGame.js
@@ -1,5 +1,6 @@
 'use strict'
 //selecting element
+
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1')
 const score0El = document.querySelector('#score--0');
@@ -92,3 +93,5 @@ const init = function() {
     diceEl.classList.add('hidden');  
 }
 btnNew.addEventListener('click', init);
+
+alert("welcomer to my dice web game.\n\nlet me walk you through the game by explaining every aspect of it.\n\nPlayers: This game can be play by either one individual against one's self or two individual against each other.\n\nRoll Dice: when you click on Roll dice, the dice will roll, and then display the number your luckily rolled.\n\nHold: when you click on Hold, your current scores will be move to total Total scores but the active player will change to the opponent.\n\nCurrent scores: This is where your scores will be compiling till you click on Hold.\n\nNew Game: when you click this, the game will reset and all value will get back to their initial.\n\n\nNote: when you roll a dice and the value is 1, your current scores will get back to Zero. so Play wisely.🤗\n\nThank you😋. Click OK to continue.");
